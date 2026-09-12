f3577ba9033c93eee40a5ee9469ec45cb256f5de
webavous <webavous@gmail.com>
2026-09-12 15:34:11 +0200

    Add AdminUsers route and component to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AdminLayout from "./pages/AdminLayout.jsx";
 import AdminCalendrier from "./pages/AdminCalendrier.jsx";
 import AdminAdherents from "./pages/AdminAdherents.jsx";
 import AdminRecap from "./pages/AdminRecap.jsx";
+import AdminUsers from "./pages/AdminUsers.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 export default function App() {
@@ -24,6 +25,7 @@ export default function App() {
         <Route path="calendrier" element={<AdminCalendrier />} />
         <Route path="adherents" element={<AdminAdherents />} />
         <Route path="recap" element={<AdminRecap />} />
+        <Route path="utilisateurs" element={<AdminUsers />} />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
